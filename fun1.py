e00de4c64680e88a938995e8519b2727b581a737
SABRIOUS <ahmedsabrious@gmail.com>
2019-12-10 22:51:52 +0200

    course_2_week_1

---
+++ fun1.py
@@ -0,0 +1,8 @@
+# Test_problem
+# olypmicsfile = open("olypmics.txt","r")
+# for aline in olypmicsfile.readlines():
+#     values = aline.split(",")
+#     Name,Sex,Age,Team,Event,Medal = values
+#     print(Name, "is from", Team, "and is on the roster for", Event)
+#
+# olypmicsfile.close()
