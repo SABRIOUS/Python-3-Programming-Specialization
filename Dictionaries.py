c50c7051a04ee0fb592af70b8c9b3d9adbe9ab07
SABRIOUS <ahmedsabrious@gmail.com>
2019-12-12 14:35:37 +0200

    week2course2

---
+++ Dictionaries.py
@@ -0,0 +1,38 @@
+# placement = "Beaches are cool places to visit in spring however the Mackinaw Bridge is near. Most people visit Mackinaw later since the island is a cool place to explore."
+# d = {}
+# for word in placement:
+#     if word not in d:
+#         d[word] = 0
+#     d[word] += 1
+# all_values = d.values()
+# min_value = ""
+# for key, value in d.items():
+#     if d[key] == min(all_values):
+#         min_value = key
+# _______________________________
+# The dictionary Junior shows a schedule for a junior year semester. The key is the course name and the value is the number of credits. Find the total number of credits taken this semester and assign it to the variable credits. Do not hardcode this – use dictionary accumulation!
+# Junior = {'SI 206':4, 'SI 310':4, 'BL 300':3, 'TO 313':3, 'BCOM 350':1, 'MO 300':3}
+# credits = sum(list(Junior.values()))
+
+# _______________________
+# Create a dictionary, freq, that displays each character in string str1 as the key and its frequency as the value.
+#
+# str1 = "peter piper picked a peck of pickled peppers"
+# freq = {}
+# for i in str1:
+#     if i not in freq:
+#         freq[i] = 0
+#     freq[i] += 1
+#
+
+# ____________________
+# just add lower() method to the string
+# string1 = "There is a tide in the affairs of men, Which taken at the flood, leads on to fortune. Omitted, all the voyage of their life is bound in shallows and in miseries. On such a full sea are we now afloat. And we must take the current when it serves, or lose our ventures.".lower()
+# letter_counts = {}
+# for i in string1:
+#     if i not in letter_counts:
+#         letter_counts[i] = 0
+#     letter_counts[i] += 1
+#
+#
+# print(letter_counts)
